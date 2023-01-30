44ff4041b62e578ce5a92b11a1149d1e9f983023
matthiasdold <matthiasdold@gmx.net>
2023-01-30 19:04:56 +0100

    Added roc curve plot

--- plotly_helpers/ml/roc.py
+++ plotly_helpers/ml/roc.py
@@ -1,2 +1,36 @@
-from scipy
+from sklearn import metrics
+
+import numpy as np
+
 import plotly.graph_objects as go
+
+
+def plot_roc_curve(y_true: np.ndarray, y_pred: np.ndarray) -> go.Figure:
+    fpr, tpr, ths = metrics.roc_curve(y_true, y_pred)
+    auc = metrics.auc(fpr, tpr)
+
+    fig = go.Figure()
+    fig.add_traces(
+        [
+
+            go.Scatter(x=[0, 1], y=[0, 1], mode='lines', name='chance',
+                       line_color='#888888'),
+            go.Scatter(x=fpr, y=tpr, mode='lines', line_color='#5555ff',
+                       name='ROC', fill='tonexty'),
+        ]
+    )
+
+    fig.add_annotation(x=0.5, y=0.8, text=f"AUC={auc}")
+
+    fig.update_layout(xaxis={'title': 'False Positive Rate'},
+                      yaxis={'title': 'True Positive Rate'})
+
+    return fig
+
+
+if __name__ == "__main__":
+    y_true = [0, 0, 0, 1, 1, 1, 0, 0, 1, 1]
+    y_pred = [0.8, 0.7, 0.3, 1.2, 1.1, 1.2, 0.8, 0.5, 1, 0.8]
+
+    fig = plot_roc_curve(y_true, y_pred)
+    fig.show()
